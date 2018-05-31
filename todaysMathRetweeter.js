cb05a8e3a4edba85457fb122289c40135b47fbbb
EC2 Default User <ec2-user@ip-172-31-85-103.ec2.internal>
2018-05-31 15:10:05 -0400

    added tags for retweet based on day

--- todaysMathRetweeter.js
+++ todaysMathRetweeter.js
@@ -4,22 +4,36 @@ var twitter = new T(twitterApiConf);
 var tweeter = require('./tweeter');
 var noget = require('noget/src/translator');
 
-/*
-
-%23supremealphabet
-%23suprememathematics %23todaysMath %23todaysMathematics
-%23nationofgodsandearths
-%23fivepercentnation
-
-*/
-
-tweeter.searchAndRetweet('%23selfknowledge');
-tweeter.searchAndRetweet('%23africanproverbs');
-tweeter.searchAndRetweet('%23blackquotes');
-tweeter.searchAndRetweet('%23zenquotes');
-//tweeter.searchAndRetweet('%23wisdom');
-//tweeter.searchAndRetweet('%23understanding');
-//tweeter.searchAndRetweet('%23suprememathematics %23todaysMath %23todaysMathematics');
-//tweeter.searchAndRetweet('%23suprememathematics %23todaysMath');
-//tweeter.searchAndRetweet('%23WednesdayWisdom');
+switch(new Date().getDay()) {
+	case 0: 
+		tweeter.searchAndRetweet('%23selfAwareness');
+		tweeter.searchAndRetweet('%2knowthyself');
+		break;
+	case 1: 
+		tweeter.searchAndRetweet('%23mindfulness');
+		tweeter.searchAndRetweet('%23knowledgeofself');
+		break;
+	case 2: 
+		tweeter.searchAndRetweet('%23showandprove');
+		tweeter.searchAndRetweet('%2faith');
+		break;
+	case 3: 
+		tweeter.searchAndRetweet('%23dotheknowledge');
+		tweeter.searchAndRetweet('%23suprememathematics');
+		break;
+	case 4: 
+		tweeter.searchAndRetweet('%23africanproverbs');
+		tweeter.searchAndRetweet('%23Wisdom');
+		break;
+	case 5: 
+		tweeter.searchAndRetweet('%2lawofattraction');
+		tweeter.searchAndRetweet('%2letgoletGod');
+		break;
+	case 6: 
+		tweeter.searchAndRetweet('%23zen');
+		tweeter.searchAndRetweet('%2thoughtsbecomethings');
+		break;
+	default: 
+		tweeter.searchAndRetweet('%23Wisdom');
+}
 
